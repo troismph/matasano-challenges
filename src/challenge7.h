31aa7b051f54f2a7631cff38aaf1350d4a80d1e7
Han <orcishhan@gmail.com>
2017-01-12 18:51:08 +0800

    temp commit for workbench transfer

--- src/challenge7.h
+++ src/challenge7.h
@@ -1,3 +1,8 @@
-int decrypt_aes_128_ecb(unsigned char* cipher, unsigned int cipher_len,
-                        unsigned char* plain,  unsigned int plain_len,
+int decrypt_aes_128_ecb(unsigned char* input, unsigned int input_len,
+                        unsigned char* output,  unsigned int output_len,
                         unsigned char* key);
+
+int encrypt_aes_128_ecb(unsigned char* input, unsigned int input_len,
+                        unsigned char* output,  unsigned int output_len,
+                        unsigned char* key);
+
