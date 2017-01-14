2f03c0b36bb36d4ac5ef07eea162395e413ef03d
g4z3 <haha@haha.com>
2017-01-14 21:13:17 +0800

    Challenge 11 pass

--- src/challenge11.py
+++ src/challenge11.py
@@ -27,13 +27,37 @@ def enc_oracle(in_buf_intact):
         tag = "cbc"
     return cipher, tag
 
+def eval_dup(buf):
+    key_len = 16
+    stat = {}
+    s = str(buf)
+    chips = [s[x : x + key_len] for x in range(0, len(s), key_len)]
+    for c in chips:
+        stat[c] = stat.get(c, 0) + 1
+    return stat
+
+def detect_stat(stat):
+    ret = {}
+    for key, val in stat.items():
+        if val > 1:
+            ret[key] = val
+    return ret
+
 def speculator(oracle_func):
     key_len = 16
     q = bytearray(65 for x in range(key_len * 4))
-
+    cipher, tag = oracle_func(q)
+    stat = eval_dup(cipher)
+    evidence = detect_stat(stat)
+    if len(evidence) > 0:
+        guess = "ecb"
+    else:
+        guess = "cbc"
+    print "{0}: guess vs. fact = {1} vs. {2}".format("Y" if guess == tag else "N", guess, tag)
 
 def run():
-    print enc_oracle(bytearray("I will be back!"))
+    for x in xrange(20):
+        speculator(enc_oracle)
 
 run()
 
