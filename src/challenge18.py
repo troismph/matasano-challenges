d4289c315dd831a3d7abbadc6a6ae3c6864adad6
Han <orcishhan@gmail.com>
2017-02-23 11:02:31 +0800

    Just a little yapf

--- src/challenge18.py
+++ src/challenge18.py
@@ -5,9 +5,11 @@ from converts import decrypt_aes_128_ctr, b64decode
 
 def run():
     key = bytearray('YELLOW SUBMARINE')
-    cipher = b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
+    cipher = b64decode(
+        'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
+    )
     plain = decrypt_aes_128_ctr(cipher, key)
     print plain
 
-run()
 
+run()
