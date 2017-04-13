19889adacbafd46683492fa366e7192c11ba684f
Han <orcishhan@gmail.com>
2017-04-13 17:35:18 +0800

    First part of challenge 24 accomplished

--- src/challenge24.py
+++ src/challenge24.py
@@ -2,6 +2,9 @@ from challenge21 import MT19937
 from converts import xor_cipher
 
 
+RNG = MT19937(879867987)
+
+
 def rng_byte_stream(seed, byte_count):
     """
     Return a random bytearray with length byte_count
@@ -26,6 +29,39 @@ def rng_byte_stream(seed, byte_count):
     return ret, rn_ref
 
 
+def recover_key_int(victim_slice, plain_chr):
+    """
+    Recover the original random number from victim slice
+    :param victim_slice: 4 bytes from victim
+    :type victim_slice: bytearray
+    :param plain_chr: the known plain character
+    :type plain_chr: str
+    :return: recovered int from the random number generator
+    :rtype: int
+    """
+    ret = 0
+    plain_byte = ord(plain_chr)
+    for i in range(4):
+        ret_slice = victim_slice[i] ^ plain_byte
+        ret = ret + (ret_slice << (i * 8))
+    return ret
+
+
+def test_rng_byte_stream():
+    seed = 98765
+    stream_len = 1024
+    bs, rn = rng_byte_stream(seed, stream_len)
+    rn_t = []
+    for x in range(0, stream_len, 4):
+        rn_t.append(recover_key_int(bs[x:x+4], '\x00'))
+    if rn != rn_t:
+        print "Failed"
+        print rn
+        print rn_t
+    else:
+        print "Pass"
+
+
 def encrypt_rng(plain, seed):
     """
     Encrypt with RNG
@@ -47,14 +83,13 @@ def decrypt_rng(cipher, seed):
 
 def test_rng_cipher():
     from os import urandom
-    rng = MT19937(998765)
     rounds = 1000
     min_len = 7
     max_len = 1024
     failed = False
     for x in range(rounds):
-        l = (rng.get_next() % max_len) + min_len
-        seed = rng.get_next() % 0xFFFF
+        l = (RNG.get_next() % max_len) + min_len
+        seed = RNG.get_next() % 0xFFFF
         plain = bytearray(urandom(l))
         cipher = encrypt_rng(plain, seed)
         plain_t = decrypt_rng(cipher, seed)
@@ -65,3 +100,33 @@ def test_rng_cipher():
             print plain_t
     if not failed:
         print "Pass after {n} rounds".format(n=rounds)
+
+
+def target(plain, seed):
+    from os import urandom
+    min_prefix_len = 7
+    max_prefix_len = 30
+    prefix_len = (RNG.get_next() % max_prefix_len) + min_prefix_len
+    prefix = bytearray(urandom(prefix_len))
+    cipher = encrypt_rng(prefix + plain, seed)
+    return cipher
+
+
+def cracker():
+    seed = RNG.get_next() & 0xFFFF
+    plain_len = 20
+    plain_txt = bytearray('V') * plain_len
+    cipher = target(plain_txt, seed)
+    for s in range(0xFFFF):
+        if s % 1000 == 0:
+            print "Trying {s}".format(s=s)
+        c = decrypt_rng(cipher, s)
+        if c[-plain_len:] == plain_txt[-plain_len:]:
+            print "Cracked, seed={s}".format(s=s)
+            if s == seed:
+                print "Crack pass"
+                return
+            else:
+                print "Crack failed, should be {s}".format(s=seed)
+                return
+    print "No crack found"
