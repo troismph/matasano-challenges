72adfda948e2dd35675e1fb5d6ab0ae7471da0c6
Han <orcishhan@gmail.com>
2017-07-02 15:14:49 +0800

    Reorganize test code of external md4 code

--- src/md4_bonsaiviking.py
+++ src/md4_bonsaiviking.py
@@ -88,22 +88,25 @@ class MD4(object):
 
 
 def test_md4():
-    if __name__ == "__main__":
-        test = (
-            ("", "31d6cfe0d16ae931b73c59d7e0c089c0"),
-            ("a", "bde52cb31de33e46245e05fbdbd6fb24"),
-            ("abc", "a448017aaf21d8525fc10ae87aa6729d"),
-            ("message digest", "d9130a8164549fe818874806e1c7014b"),
-            ("abcdefghijklmnopqrstuvwxyz", "d79e1c308aa5bbcdeea8ed63df412da9"),
-            ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "043f8582f241db351ce627e153e7f0e4"),
-            ("12345678901234567890123456789012345678901234567890123456789012345678901234567890",
-             "e33b4ddc9c38f2199c3e7b164fcc0536")
-        )
-        for t, h in test:
-            md = MD4()
-            md.add(t)
-            d = md.finish()
-            if d == h.decode("hex"):
-                print "pass"
-            else:
-                print "FAIL: {0}: {1}\n\texpected: {2}".format(t, d.encode("hex"), h)
+    test = (
+        ("", "31d6cfe0d16ae931b73c59d7e0c089c0"),
+        ("a", "bde52cb31de33e46245e05fbdbd6fb24"),
+        ("abc", "a448017aaf21d8525fc10ae87aa6729d"),
+        ("message digest", "d9130a8164549fe818874806e1c7014b"),
+        ("abcdefghijklmnopqrstuvwxyz", "d79e1c308aa5bbcdeea8ed63df412da9"),
+        ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "043f8582f241db351ce627e153e7f0e4"),
+        ("12345678901234567890123456789012345678901234567890123456789012345678901234567890",
+         "e33b4ddc9c38f2199c3e7b164fcc0536")
+    )
+    for t, h in test:
+        md = MD4()
+        md.add(t)
+        d = md.finish()
+        if d == h.decode("hex"):
+            print "pass"
+        else:
+            print "FAIL: {0}: {1}\n\texpected: {2}".format(t, d.encode("hex"), h)
+
+
+if __name__ == '__main__':
+    test_md4()
